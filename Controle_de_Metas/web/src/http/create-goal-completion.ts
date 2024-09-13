42a3b6b06d071abbbd8b125032340b884383c34f
Matheus Douglas Alves Vitor <matheusdouglas58@gmail.com>
2024-09-13 08:53:00 -0300

    Versão Final
    Projeto desenvolvido nas linguagens:
    
    Frontend: React
    Backend: NodeJS

---
+++ Controle_de_Metas/web/src/http/create-goal-completion.ts
@@ -0,0 +1,23 @@
+export interface CreateGoalCompletionRequest {
+    goalId: string
+  }
+  
+  export async function createGoalCompletion({
+    goalId,
+  }: CreateGoalCompletionRequest): Promise<void> {
+    const response = await fetch('http://localhost:3333/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        goalId,
+      }),
+    })
+
+    console.log(response)
+  
+    if (!response.ok) {
+      throw new Error('Error while creating the goal completion')
+    }
+  }
